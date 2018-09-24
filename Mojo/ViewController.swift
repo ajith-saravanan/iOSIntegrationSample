44720dbdf97d617054c3b12d6c091c2ef0b462c6
Mithilesh Sinha <mithilesh@mithileshs-macbook-air.local>
2018-09-24 11:58:31 +0530

    Initial Commit

---
+++ Mojo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mojo
+//
+//  Created by Mithilesh Sinha on 24/09/18.
+//  Copyright © 2018 mirab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
